60ca586bd16b7a9bfb8f0d8f54f2e2f9edbb9c60
Salvador Guzman <guzmansalv@gmail.com>
2017-12-08 07:46:15 -0800

    simplified query list - increased cluster output

--- src/main/scala/com/github/sguzman/scala/have/scourage/Main.scala
+++ src/main/scala/com/github/sguzman/scala/have/scourage/Main.scala
@@ -1,9 +1,14 @@
 package com.github.sguzman.scala.have.scourage
 
+import com.github.sguzman.scala.have.scourage.typesafe.Agency
 import fr.hmil.roshttp.HttpRequest
+
+import io.circe.parser.decode
+import io.circe.generic.auto._
 
 import scala.concurrent.Await
 import monix.execution.Scheduler.Implicits.global
+
 import scala.concurrent.duration.Duration
 
 object Main {
@@ -11,41 +16,18 @@ object Main {
     val request = get
     val response = Await.result(request.send, Duration.Inf)
     val body = response.body
-    println(body)
+    val agency = decode[Agency](body).right.get
+    val clusters = agency.agencies.head.clusters
+    println(clusters.length)
   }
 
   def get: HttpRequest = HttpRequest("https://www.crimereports.com/api/crimes/cluster.json?agency_id=33")
-    .withQueryParameter("days", "sunday,monday,tuesday,wednesday,thursday,friday,saturday" )
     .withQueryParameter("end_date", "2017-12-07")
-    .withQueryParameter("end_time", "23")
-    .withQueryParameter("incident_types", "Assault,Assault" +
-      "+with" +
-      "+Deadly" +
-      "+Weapon,Breaking" +
-      "+%26" +
-      "+Entering,Disorder,Drugs,Homicide,Kidnapping,Liquor,Other" +
-      "+Sexual" +
-      "+Offense,Property" +
-      "+Crime,Property" +
-      "+Crime" +
-      "+Commercial,Property" +
-      "+Crime" +
-      "+Residential,Quality" +
-      "+of" +
-      "+Life,Robbery,Sexual" +
-      "+Assault,Sexual" +
-      "+Offense,Theft,Theft" +
-      "+from" +
-      "+Vehicle,Theft" +
-      "+of" +
-      "+Vehicle")
     .withQueryParameter("include_sex_offenders", "false")
-    .withQueryParameter("lat1", "37.612055711412815")
-    .withQueryParameter("lat2", "36.787291466820015")
-    .withQueryParameter("lng1", "-119.84710693359375")
-    .withQueryParameter("lng2", "-123.80218505859375")
-    .withQueryParameter("sandbox", "false")
+    .withQueryParameter("lat1", "37.490455")
+    .withQueryParameter("lng1", "-122.200928")
+    .withQueryParameter("lat2", "37.217548")
+    .withQueryParameter("lng2", "-121.701050")
     .withQueryParameter("start_date", "2016-01-01")
-    .withQueryParameter("start_time", "0")
-    .withQueryParameter("zoom", "9")
+    .withQueryParameter("zoom", "14")
 }
