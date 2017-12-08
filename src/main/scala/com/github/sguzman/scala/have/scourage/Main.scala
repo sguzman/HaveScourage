01d461a80ecb5b45842e98412d79d5b54d7f92b1
Salvador Guzman <guzmansalv@gmail.com>
2017-12-08 11:08:38 -0800

    stub main for scala.js

--- src/main/scala/com/github/sguzman/scala/have/scourage/Main.scala
+++ src/main/scala/com/github/sguzman/scala/have/scourage/Main.scala
@@ -1,33 +1,10 @@
 package com.github.sguzman.scala.have.scourage
 
-import com.github.sguzman.scala.have.scourage.typesafe.Agency
-import fr.hmil.roshttp.HttpRequest
-
-import io.circe.parser.decode
-import io.circe.generic.auto._
-
-import scala.concurrent.Await
-import monix.execution.Scheduler.Implicits.global
-
-import scala.concurrent.duration.Duration
-
 object Main {
   def main(args: Array[String]): Unit = {
-    val request = get
-    val response = Await.result(request.send, Duration.Inf)
-    val body = response.body
-    val agency = decode[Agency](body).right.get
-    val clusters = agency.agencies.head.clusters
-    println(clusters.length)
+    println(url)
   }
 
-  def get: HttpRequest = HttpRequest("https://www.crimereports.com/api/crimes/cluster.json?agency_id=33")
-    .withQueryParameter("end_date", "2017-12-07")
-    .withQueryParameter("include_sex_offenders", "false")
-    .withQueryParameter("lat1", "37.490455")
-    .withQueryParameter("lng1", "-122.200928")
-    .withQueryParameter("lat2", "37.217548")
-    .withQueryParameter("lng2", "-121.701050")
-    .withQueryParameter("start_date", "2016-01-01")
-    .withQueryParameter("zoom", "14")
+  def url: String =
+    "https://www.crimereports.com/api/crimes/cluster.json?agency_id=33&end_date=2017-12-08&include_sex_offenders=false &lat1=37.490455&lng1=-122.200928&lat2=37.217548 &lng2=-121.701050&start_date=2016-01-01&zoom=14"
 }
